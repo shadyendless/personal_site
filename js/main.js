d08f3e55a0d5e4a169bce93a14f86acf988eef32
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-02-24 22:46:39 -0800

    Added a github link and the copyright to the footer.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 (function () {
-    const hamburgerIcon = document.querySelector('.hamburger');
-    const mobileMenu = document.querySelector('.nav-items__mobile');
+    document.addEventListener('DOMContentLoaded', () => {
+        const hamburgerIcon = document.querySelector('.hamburger');
+        const mobileMenu = document.querySelector('.nav-items__mobile');
 
-    hamburgerIcon.addEventListener('click', (event) => {
-        event.preventDefault();
+        hamburgerIcon.addEventListener('click', (event) => {
+            event.preventDefault();
 
-        hamburgerIcon.classList.toggle('open');
-        mobileMenu.classList.toggle('is-none');
+            hamburgerIcon.classList.toggle('open');
+            mobileMenu.classList.toggle('is-none');
+        });
+
+
+        document.querySelector('.copyright-year').innerHTML = (new Date()).getFullYear();
     });
 }());
