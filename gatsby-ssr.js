6d1a4e3046e9d94a9950d2875526d9cac2459f3f
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-03-01 14:46:25 -0800

    Web fonts are now loaded in the background and then added once they are fetched.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,13 @@
  */
 import React from 'react';
 
-exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }, pluginOptions) => {
+    setHeadComponents([
+        <link rel="preload" href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700,900" as="style" onload="this.rel='stylesheet'" />,
+        <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700,900" /></noscript>,
+        <link rel="preload" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i" as="style" onload="this.rel='stylesheet'" />,
+        <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i" /></noscript>
+    ]);
     setPostBodyComponents([
         <script key="polyfill-io" src="https://cdn.polyfill.io/v2/polyfill.min.js?features=IntersectionObserver&gated"></script>
     ]);
